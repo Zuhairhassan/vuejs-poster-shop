3a35373824ba030ff0df9836f2e1721a9a7a34c9
zuhair <Zuhair@yahoo.com>
2019-09-19 11:17:00 +0500

    addproduct into card arrayt

--- public/script.js
+++ public/script.js
@@ -3,15 +3,16 @@ el:"#app",
 data:{
     total:0,
     products:[
-     {title :'product1',id:1},
-     {title :'product2',id:2},
-     {title :'product3',id:3},
-
-    ]
+     {title :'product1',id:1, price:1.44},
+     {title :'product2',id:2 ,price:2.13},
+     {title :'product3',id:3, price:3.23},
+    ],
+    cart:[]
 },
 methods:{
     addToCart:function(product){
-        console.log(product.id);
+        this.total +=product.price;
+        this.cart.push(product);
     }
 }
 });
