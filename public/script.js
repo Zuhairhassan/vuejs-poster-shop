5702962337c2fb6549a74eb282803d2b7324c0e3
zuhair <Zuhair@yahoo.com>
2019-09-19 10:55:29 +0500

    making the first project now in stage of addingcart

--- public/script.js
+++ public/script.js
@@ -1,1 +1,17 @@
-console.log('It works.');
+new Vue({
+el:"#app",
+data:{
+    total:0,
+    products:[
+     {title :'product1',id:1},
+     {title :'product2',id:2},
+     {title :'product3',id:3},
+
+    ]
+},
+methods:{
+    addToCart:function(){
+        this.total +=9.99;
+    }
+}
+});
